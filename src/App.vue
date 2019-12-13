6b765c47f26ea4831e04f068a3680fceb530b155
yangxiaofei <770361563@qq.com>
2019-12-13 11:27:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+   <p>sfdsdf saf </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import './common/stylus/mixins.styl';
+p
+  color red
+</style>
+
